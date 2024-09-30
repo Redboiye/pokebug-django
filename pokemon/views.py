4778fc18db2df2cde8d8aebf2174496cd8a53395
Kaspars <smarnss@gmail.com>
2024-09-30 19:14:17 +0300

    add pokemon_details

--- pokemon/views.py
+++ pokemon/views.py
@@ -6,28 +6,41 @@ from .models import Pokemon
 # request ir metadati, url, sakara ar backend komunikaciju
 def home(request):
     url = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
+    # get metode
     response = requests.get(url)
+    # kad mes sutam request(jebkada metode) mes sanemam atbildi ka response, nak Json valoda.
+    # lai python varetu saprast info, mums vajag partulkot json un konkreto python data type.
+    # dictionary ir type.
     data = response.json()
     results = data.get("results")
 
     for pokemon in results:
+        # elementi saraksta #atslega + vertiba veido dictionary
+        # visur kur ir figut iekavas ir dictionary, izgustot vertiba ar .get pierakstot atslegu,
+        # taja geta
+        # no sava pirma objekta dabusi name, urli
         name = pokemon.get("name").capitalize()
+
         pokemon_details_url = pokemon.get("url")
         pokemon_detail_response = requests.get(pokemon_details_url)
         pokemon_detail_data = pokemon_detail_response.json()
         pokemon_types = pokemon_detail_data.get("types")
-        print(pokemon_types)
-
-
+        pokemon_detail_type = pokemon_types[0].get("type").get("name")
+        pokemon_detail_artwork = pokemon_detail_data.get("sprites").get("other").get("official-artwork").get(
+            "front_default")
+        print(name, pokemon_detail_type, pokemon_detail_artwork)
         Pokemon.objects.update_or_create(
-            name=name, url=pokemon_details_url,
+            name=name,
+            url=pokemon_details_url,
+            type=pokemon_detail_type,
+            picture=pokemon_detail_artwork,
 
         )
 
-    # ORM metode all pasauc objektus no Pokemon
-    pokemons = Pokemon.objects.all()
-    # context pado informaciju uz html un ir pieejama ar conteksta atslegam
-    context = {"all_pokemons": pokemons}
+        # ORM metode all pasauc objektus no Pokemon
+        pokemons = Pokemon.objects.all()
+        # context pado informaciju uz html un ir pieejama ar conteksta atslegam
+        context = {"all_pokemons": pokemons}
 
     return render(request, "pokemon/index.html", context)
 
